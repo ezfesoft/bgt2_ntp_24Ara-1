dd3d74b561645e5a50c10e2462e4df3beecb5171
slymn_LAB2 <ezfesoftware@gmail.com>
2024-12-24 09:15:34 +0300

    Proje dosyası ekle.

---
+++ bgt2_ntp_24Ara-1/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using FireSharp.Config;
+using FireSharp.Interfaces;
+
+namespace bgt2_ntp_24Ara_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        IFirebaseConfig config = new FirebaseConfig
+        {
+            AuthSecret = "3mkw7KGTqF2yJNn9IWyM78WlzGN9vhCdXIARwuRW",
+            BasePath = "https://bgt2-ntpii-24-25-default-rtdb.europe-west1.firebasedatabase.app/"
+        };
+        IFirebaseClient client;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            client = new FireSharp.FirebaseClient(config);
+            if (client != null)
+            {
+                MessageBox.Show("Bağlantı Başarılı");
+                label1.Visible = true;
+            }
+        }
+    }
+}
---
+++ bgt2_ntp_24Ara-1/Ogrenci.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bgt2_ntp_24Ara_1
+{
+    public class Ogrenci
+    {
+        public int Id { get; set; }
+        public string Ad { get; set; }
+        public string Soyad { get; set; }
+        public string Numara { get; set; }
+        public string Sinif { get; set; }
+        public string Sube { get; set; }    
+        public string Developer  {get;set;}
+    }
+}
