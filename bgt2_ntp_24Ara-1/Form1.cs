06c57efe00bb385ddcdc6dbd4ecbc2d2c7e5bb4f
slymn_LAB2 <ezfesoftware@gmail.com>
2024-12-24 09:58:31 +0300

    v2

--- bgt2_ntp_24Ara-1/Form1.cs
+++ bgt2_ntp_24Ara-1/Form1.cs
@@ -9,6 +9,8 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using FireSharp.Config;
 using FireSharp.Interfaces;
+using FireSharp.Response;
+using Newtonsoft.Json;
 
 namespace bgt2_ntp_24Ara_1
 {
@@ -35,5 +37,59 @@ namespace bgt2_ntp_24Ara_1
                 label1.Visible = true;
             }
         }
+
+        SetResponse istek = null;
+        Ogrenci ogr = null;
+
+        private async void button2_Click(object sender, EventArgs e)
+        {
+
+            var ogrenci = new Ogrenci
+            {
+                Id = Convert.ToInt32(textBox1.Text),
+                Ad = textBox2.Text,
+                Soyad = textBox3.Text,
+                Numara = textBox4.Text,
+                Sinif = textBox5.Text,
+                Sube = textBox6.Text,
+                Developer = "slymn"
+            };
+
+            istek=await client.SetAsync("Ogrenci/" + ogrenci.Id, ogrenci);
+            ogr = istek.ResultAs<Ogrenci>();
+                       
+            if (ogr.Id != null)
+            {
+                MessageBox.Show("Ekleme Başarılı");
+                textBox1.Clear();
+                textBox2.Clear();
+                textBox3.Clear();
+                textBox4.Clear();
+                textBox5.Clear();
+                textBox6.Clear();
+            }
+            else
+            {
+                MessageBox.Show("Ekleme Başarısız");
+            }
+        }
+
+        List<Ogrenci> ogrenciler = new List<Ogrenci>();
+        FirebaseResponse cevap=null;
+        private async void button3_Click(object sender, EventArgs e)
+        {
+            cevap = await client.GetAsync("Ogrenci");
+            var sonuc = cevap.Body;
+            var data = JsonConvert.DeserializeObject<Dictionary<string, Ogrenci>>(sonuc);
+
+            foreach (var item in data)
+            {
+                Ogrenci ogr = item.Value;
+                ogrenciler.Add(ogr);
+                listBox1.Items.Add(item.Value.Id + " " + item.Value.Ad + " " + item.Value.Soyad + " " + item.Value.Numara + " " + item.Value.Sinif + " " + item.Value.Sube);
+            }
+
+        }
     }
+
 }
